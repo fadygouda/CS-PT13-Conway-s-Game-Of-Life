298dd907203e323702082fc67ee3b23330a66780
fadygouda <fadygouda123@gmail.com>
2020-10-19 19:38:03 -0700

    trying anohter fix

--- gameoflife/src/components/grid.js
+++ gameoflife/src/components/grid.js
@@ -21,7 +21,7 @@ class Box extends React.Component {
 
 class Grid extends React.Component {
     render() {
-        const width = this.props.cols * 16;
+        const width = this.props.cols * 14;
         var rowsArr = [];
 
         var boxClass = "";
